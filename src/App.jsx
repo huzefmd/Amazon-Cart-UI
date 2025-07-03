29f6c3de280fcc171ea8e52ec1fe9a1861d71562
huzefmd <huzefmd686@gmail.com>
2025-07-03 23:47:31 +0530

    zustand-init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AmazonStyleCart from './components/AmazonStyleCart'
+
+function App() {
+  return (
+    <div>
+      <AmazonStyleCart/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/AmazonStyleCart.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useCartItemStore } from '../store/cartItemState';
+function AmazonStyleCart() {
+  const cartItems = useCartItemStore((state) => state.cartItems);
+  const remove = useCartItemStore((state) => state.removeItem);
+  const update = useCartItemStore((state) => state.updateQuantity);
+  const total = useCartItemStore((state) => state.gettotalPrice());
+  return (
+    <div>
+      {cartItems.map((item) => (
+        <div key={item.id}>
+          <img src={item.image} />
+          <h3>{item.name}</h3>
+          <h3>{item.quantity}</h3>
+          <button onClick={() => { update(item.id, item.quantity + 1) }}>+</button>
+          <button onClick={()=>{
+            if (item.quantity > 1) {
+              update(item.id ,item.quantity-1)
+            }
+          }}>-</button>
+          <p>{item.price}</p>
+          <button onClick={remove}>Remove</button>
+        </div>
+      ))}
+      <div>
+        Total is { total}
+      </div>
+    </div>
+  );
+}
+
+export default AmazonStyleCart
